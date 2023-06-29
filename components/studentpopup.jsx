17f040bb4ed50891089f21dffab2302b5586790f
arifmehniyar <arif.mehniyar@gmail.com>
2023-06-30 02:39:39 +0300

    create popup

--- components/studentpopup.jsx
+++ components/studentpopup.jsx
@@ -9,20 +9,20 @@ export const StudentPopup = ({onClose}) => {
     <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div
-                className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full md:w-1/2 md:max-w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                     <div className="sm:flex sm:items-start">
                         <div
-                            className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                 stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"/>
+                            className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-300 sm:mx-0 sm:h-10 sm:w-10">
+                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                <path
+                                    d="M6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Zm11-3h-2V5a1 1 0 0 0-2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 0 0 2 0V9h2a1 1 0 1 0 0-2Z"/>
                             </svg>
                         </div>
                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                            <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate
-                                account</h3>
+                            <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Create
+                                new student</h3>
                             <div className="mt-2">
                                 <p className="text-sm text-gray-500">Are you sure you want to deactivate your account?
                                     All of your data will be permanently removed. This action cannot be undone.</p>
@@ -30,9 +30,94 @@ export const StudentPopup = ({onClose}) => {
                         </div>
                     </div>
                 </div>
+
+                <form className="p-12">
+                    <div className="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <label htmlFor="first_name"
+                                   className="block mb-2 text-sm font-medium text-gray-900 ">First
+                                name</label>
+                            <input type="text" id="first_name"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                   placeholder="John" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="last_name"
+                                   className="block mb-2 text-sm font-medium text-gray-900 ">Last
+                                name</label>
+                            <input type="text" id="last_name"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                   placeholder="Doe" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="company"
+                                   className="block mb-2 text-sm font-medium text-gray-900 ">Company</label>
+                            <input type="text" id="company"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                   placeholder="ManageCourses" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="phone"
+                                   className="block mb-2 text-sm font-medium text-gray-900 ">Phone
+                                number</label>
+                            <input type="tel" id="phone"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                   placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="website"
+                                   className="block mb-2 text-sm font-medium text-gray-900 ">Website
+                                URL</label>
+                            <input type="url" id="website"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                   placeholder="managecourses.com" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="visitors"
+                                   className="block mb-2 text-sm font-medium text-gray-900 ">Unique
+                                visitors (per month)</label>
+                            <input type="number" id="visitors"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                   placeholder="" required/>
+                        </div>
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Email
+                            address</label>
+                        <input type="email" id="email"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                               placeholder="john.doe@company.com" required/>
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="password"
+                               className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                        <input type="password" id="password"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                               placeholder="•••••••••" required/>
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="confirm_password"
+                               className="block mb-2 text-sm font-medium text-gray-900 ">Confirm
+                            password</label>
+                        <input type="password" id="confirm_password"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                               placeholder="•••••••••" required/>
+                    </div>
+                    <div className="flex items-start mb-6">
+                        <div className="flex items-center h-5">
+                            <input id="remember" type="checkbox" value=""
+                                   className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                                   required/>
+                        </div>
+                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I
+                            agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms
+                                and conditions</a>.</label>
+                    </div>
+                </form>
+
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                     <button type="button"
-                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate
+                            className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto">Apply
                     </button>
                     <button type="button" onClick={onClose}
                             className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel
