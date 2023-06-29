1db1307f00740349c5cf2c0239cf21aec33302df
arifmehniyar <arif.mehniyar@gmail.com>
2023-06-30 02:44:02 +0300

    create popup

--- components/studentpopup.jsx
+++ components/studentpopup.jsx
@@ -24,8 +24,7 @@ export const StudentPopup = ({onClose}) => {
                             <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Create
                                 new student</h3>
                             <div className="mt-2">
-                                <p className="text-sm text-gray-500">Are you sure you want to deactivate your account?
-                                    All of your data will be permanently removed. This action cannot be undone.</p>
+                                <p className="text-sm text-gray-500">Please enter the student information you want to add completely below.</p>
                             </div>
                         </div>
                     </div>
