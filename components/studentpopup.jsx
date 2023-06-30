90c876d4708e198d7fa27be0a7bebc7d00ee9613
arifmehniyar <arif.mehniyar@gmail.com>
2023-06-30 23:15:23 +0300

    form validation

--- components/studentpopup.jsx
+++ components/studentpopup.jsx
@@ -1,11 +1,64 @@
 "use client"
 
 
+import {useEffect, useState} from "react";
+
 export const StudentPopup = ({onClose}) => {
+    const [isFormValid, setIsFormValid] = useState(false)
+    const [validationError, setValidationError] = useState("")
+    const [formData, setFormData] = useState({
+        firstname: 'test',
+        lastname: '',
+        email: '',
+        phone: '',
+        website: '',
+        companyName: '',
+        password: '',
+        passwordConfirm: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+
+        handleValidationError(e)
+        setIsFormValid([...document.getElementById("create-new-student-form").querySelectorAll('input')].every((el) => el.checkValidity()))
+    };
+
+    const handleValidationError = (e, isReport=false) => {
+        const { name, value } = e.target;
+
+        queueMicrotask(()=>{
+            if(name === 'passwordConfirm' && !!value && document.querySelector("#password").value === value){
+                e.target.setCustomValidity("")
+                setValidationError("")
+            }else if(e.target.value && !e.target.checkValidity()){
+                if(isReport){
+                    e.target.reportValidity()
+                }
+                setValidationError(e.target.validationMessage)
+            }else if(name === 'passwordConfirm' && !!value && document.querySelector("#password").value !== value){
+                e.target.setCustomValidity("Passwords Not Match")
+                setValidationError(e.target.validationMessage)
+            }else{
+                e.target.setCustomValidity("")
+                setValidationError("")
+            }
+        })
+
+        setIsFormValid([...document.getElementById("create-new-student-form").querySelectorAll('input')].every((el) => el.checkValidity()))
+    };
+
+    const handleBlur = (e) => {
+        handleValidationError(e, true)
+    };
+
     return (
 <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
     <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div
@@ -30,13 +83,18 @@ export const StudentPopup = ({onClose}) => {
                     </div>
                 </div>
 
-                <form className="p-12">
+                <form className="p-12" id="create-new-student-form">
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                         <div>
                             <label htmlFor="first_name"
                                    className="block mb-2 text-sm font-medium text-gray-900 ">First
                                 name</label>
                             <input type="text" id="first_name"
+                                   value={formData.firstname}
+                                   minLength={2}
+                                   name="firstname"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                    placeholder="John" required/>
                         </div>
@@ -45,6 +103,11 @@ export const StudentPopup = ({onClose}) => {
                                    className="block mb-2 text-sm font-medium text-gray-900 ">Last
                                 name</label>
                             <input type="text" id="last_name"
+                                   name="lastname"
+                                   value={formData.lastname}
+                                   minLength={2}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                    placeholder="Doe" required/>
                         </div>
@@ -52,6 +115,11 @@ export const StudentPopup = ({onClose}) => {
                             <label htmlFor="company"
                                    className="block mb-2 text-sm font-medium text-gray-900 ">Company</label>
                             <input type="text" id="company"
+                                   name="companyName"
+                                   value={formData.companyName}
+                                   minLength={2}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                    placeholder="ManageCourses" required/>
                         </div>
@@ -60,6 +128,10 @@ export const StudentPopup = ({onClose}) => {
                                    className="block mb-2 text-sm font-medium text-gray-900 ">Phone
                                 number</label>
                             <input type="tel" id="phone"
+                                   name="phone"
+                                   value={formData.phone}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                    placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required/>
                         </div>
@@ -67,23 +139,24 @@ export const StudentPopup = ({onClose}) => {
                             <label htmlFor="website"
                                    className="block mb-2 text-sm font-medium text-gray-900 ">Website
                                 URL</label>
-                            <input type="url" id="website"
+                            <input type="text" id="website"
+                                   name="website"
+                                   value={formData.website}
+                                   onChange={handleChange}
+                                   pattern="^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$"
+                                   onBlur={handleBlur}
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                    placeholder="managecourses.com" required/>
-                        </div>
-                        <div>
-                            <label htmlFor="visitors"
-                                   className="block mb-2 text-sm font-medium text-gray-900 ">Unique
-                                visitors (per month)</label>
-                            <input type="number" id="visitors"
-                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                                   placeholder="" required/>
                         </div>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Email
                             address</label>
                         <input type="email" id="email"
+                               value={formData.email}
+                               name="email"
+                               onChange={handleChange}
+                               onBlur={handleBlur}
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                placeholder="john.doe@company.com" required/>
                     </div>
@@ -91,6 +164,11 @@ export const StudentPopup = ({onClose}) => {
                         <label htmlFor="password"
                                className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
                         <input type="password" id="password"
+                               name="password"
+                               value={formData.password}
+                               minLength={8}
+                               onChange={handleChange}
+                               onBlur={handleBlur}
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                placeholder="•••••••••" required/>
                     </div>
@@ -99,27 +177,30 @@ export const StudentPopup = ({onClose}) => {
                                className="block mb-2 text-sm font-medium text-gray-900 ">Confirm
                             password</label>
                         <input type="password" id="confirm_password"
+                               name="passwordConfirm"
+                               minLength={8}
+                               value={formData.passwordConfirm}
+                               onChange={handleChange}
+                               onBlur={handleBlur}
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                placeholder="•••••••••" required/>
                     </div>
-                    <div className="flex items-start mb-6">
-                        <div className="flex items-center h-5">
-                            <input id="remember" type="checkbox" value=""
-                                   className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                                   required/>
-                        </div>
-                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I
-                            agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms
-                                and conditions</a>.</label>
-                    </div>
+
+                    {validationError &&
+                    <div
+                        className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
+                        role="alert">
+                        <span className="font-medium">Form Validation alert!</span> {validationError}
+                    </div>}
                 </form>
 
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                     <button type="button"
-                            className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto">Apply
+                            disabled={!isFormValid}
+                            className="inline-flex w-full justify-center cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto">Apply
                     </button>
                     <button type="button" onClick={onClose}
-                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel
+                            className="mt-3 inline-flex w-full  justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel
                     </button>
                 </div>
             </div>
