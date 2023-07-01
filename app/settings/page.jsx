238f9ed131fdba23f5b614054c67491188df44ac
arifmehniyar <arif.mehniyar@gmail.com>
2023-07-01 15:43:53 +0300

    fix settings page

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -4,8 +4,8 @@ export default function SettingsPage() {
 
     return (
         <MainLayout>
-            <div className="flex h-full flex-col items-center justify-between">
-                <img className="w-full md:w-1/2 h-full" src="/prerequition.png"/>
+            <div className="flex h-full md:w-1/2 flex-col items-center justify-between pt-[62px]">
+                <img className="w-full h-auto" src="/prerequition.png"/>
             </div>
         </MainLayout>
     );
